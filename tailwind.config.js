42313eca3364d11adaa62235418fe483f41d03f1
Mostafa Hamdy <mostafa.hamdy.abdo@gmail.com>
2022-12-10 02:05:34 +0200

    chore: initiate project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    fontFamily: {
+      aHelvetica: 'Arabic Helvetica'
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
